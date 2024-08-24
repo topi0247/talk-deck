ab92476efd5a4c3461b1c8e9efbd9e4a65d6c28c
topi0247 <karubona.ra247@gmail.com>
2024-08-24 21:28:02 +0900

    feat:メインページレイアウト

--- front/src/app/(main)/layout.tsx
+++ front/src/app/(main)/layout.tsx
@@ -1,16 +1,9 @@
-import { Footer, Header } from "@/components/layout";
-import Link from "next/link";
+import { MainLayout } from "@/components/layout";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen w-full bg-blue-200">
-      <Header />
-      <main className="flex-grow w-full container p-4 md:p-0">{children}</main>
-      <Footer />
-    </div>
-  );
+  return <MainLayout>{children}</MainLayout>;
 }
