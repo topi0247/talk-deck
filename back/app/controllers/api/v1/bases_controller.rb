1225bef11ac733ad740a2c273b1baf5571f8f1cb
topi0247 <karubona.ra247@gmail.com>
2024-08-28 16:23:36 +0900

    feat:バックエンドの認証実装

---
+++ back/app/controllers/api/v1/bases_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::BasesController < ApplicationController
+  before_action :authenticate!
+
+  def authenticate!
+    token = request.headers['Authorization']
+    decode = Jwt.decode(token.split(' ').last).first
+    expired = decode['expired']
+    if expired + 2.weeks < Time.now.to_i
+      render json: { error: '認証期間切れです' }, status: :unauthorized
+      return
+    end
+    user_id = decode['user_id']
+    @current_user = User.find_by(id: user_id)
+  end
+end
