f3fd7772884529b90c80978a4cb02e9dedc2bca2
topi0247 <karubona.ra247@gmail.com>
2024-08-28 16:35:21 +0900

    fix:ヘッダーをログイン状態に応じた表示に修正

--- front/src/components/layout/header/index.tsx
+++ front/src/components/layout/header/index.tsx
@@ -4,19 +4,22 @@ import { useDisclosure } from "@mantine/hooks";
 import { Burger, Drawer } from "@mantine/core";
 import { SiteName } from "@/components/layout";
 import { CurrentUser } from "@/feature/auth";
+import { userState } from "@/recoil";
+import { useRecoilValue } from "recoil";
 
 export default function Header() {
   const [opened, { toggle }] = useDisclosure();
+  const user = useRecoilValue(userState);
 
   return (
     <>
       <CurrentUser />
       <header className="relative w-full pl-2 pt-2">
-        <Link href="/">
+        <Link href={user.name ? "/talkDeck" : "/"}>
           <SiteName />
         </Link>
         <nav className="hidden md:block md:fixed bottom-0 left-0 w-full z-50 px-8 bg-black bg-opacity-15">
-          <ul className="flex w-full gap-8 max-w-[1000px] m-auto">
+          <ul className="flex w-full gap-8 max-w-[1000px] m-auto justify-center items-center">
             <li className="w-1/4 h-28">
               <Link
                 href="/talkDeck"
@@ -29,54 +32,58 @@ export default function Header() {
                 </span>
               </Link>
             </li>
-            <li className="w-1/4 h-28">
-              <Link
-                href="/talkDeck/new"
-                className="bg-blue-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
-              >
-                <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
-                  新規作成
-                  <br />
-                  新しいデッキを作成
-                </span>
-              </Link>
-            </li>
-            <li className="w-1/4 h-28">
-              <Link
-                href="/talkDeck/mydeck"
-                className="bg-blue-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
-              >
-                <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
-                  マイデッキ
-                  <br />
-                  自分のデッキ
-                </span>
-              </Link>
-            </li>
-            <li className="w-1/4 h-28">
-              <Link
-                href="/talkDeck/likes"
-                className="bg-green-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
-              >
-                <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
-                  お気に入り
-                  <br />
-                  お気に入り
-                </span>
-              </Link>
-            </li>
-            <li className="w-1/4 h-28">
-              <Link
-                href="/mypage"
-                className="bg-yellow-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
-              >
-                <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
-                  マイページ
-                  <br />
-                  ユーザー設定
-                </span>
-              </Link>
-            </li>
+            {user.name && (
+              <>
+                <li className="w-1/4 h-28">
+                  <Link
+                    href="/talkDeck/new"
+                    className="bg-blue-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
+                  >
+                    <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
+                      新規作成
+                      <br />
+                      新しいデッキを作
+                    </span>
+                  </Link>
+                </li>
+                <li className="w-1/4 h-28">
+                  <Link
+                    href="/talkDeck/mydeck"
+                    className="bg-blue-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
+                  >
+                    <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
+                      マイデッキ
+                      <br />
+                      自分のデッキ
+                    </span>
+                  </Link>
+                </li>
+                <li className="w-1/4 h-28">
+                  <Link
+                    href="/talkDeck/likes"
+                    className="bg-green-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
+                  >
+                    <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
+                      お気に入り
+                      <br />
+                      お気に入り
+                    </span>
+                  </Link>
+                </li>
+                <li className="w-1/4 h-28">
+                  <Link
+                    href="/mypage"
+                    className="bg-yellow-300 px-2 pt-2 flex justify-center items-center rounded w-full transform translate-y-12 hover:-translate-y-0 transition-all h-full"
+                  >
+                    <span className="bg-white h-full text-black w-full px-4 py-4 rounded text-center leading-10">
+                      マイページ
+                      <br />
+                      ユーザー設定
+                    </span>
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
         <div className="absolute right-0 top-0 m-4 bg-white rounded-full w-12 h-12 flex justify-center items-center z-50 md:hidden">
@@ -96,6 +103,13 @@ export default function Header() {
             <ul className="flex flex-col justify-center items-center h-full w-full">
               <li className="w-full text-center">
                 <Link
+                  href="/"
+                  className="bg-yellow-100 w-full p-4 flex justify-center items-center"
+                  style={{ lineHeight: "1.5" }}
+                >
+                  TOP
+                </Link>
+                <Link
                   href="/talkDeck"
                   className="bg-yellow-100 w-full p-4 flex justify-center items-center"
                   style={{ lineHeight: "1.5" }}
@@ -103,42 +117,46 @@ export default function Header() {
                   一覧
                 </Link>
               </li>
-              <li className="w-full text-center">
-                <Link
-                  href="/talkDeck/new"
-                  className="bg-yellow-100 w-full p-4 flex justify-center items-center"
-                  style={{ lineHeight: "1.5" }}
-                >
-                  新規作成
-                </Link>
-              </li>
-              <li className="w-full text-center">
-                <Link
-                  href="/talkDeck/mydeck"
-                  className="bg-yellow-100 w-full p-4 flex justify-center items-center"
-                  style={{ lineHeight: "1.5" }}
-                >
-                  マイデッキ
-                </Link>
-              </li>
-              <li className="w-full text-center">
-                <Link
-                  href="/talkDeck/likes"
-                  className="bg-yellow-100 w-full p-4 flex justify-center items-center"
-                  style={{ lineHeight: "1.5" }}
-                >
-                  お気に入り
-                </Link>
-              </li>
-              <li className="w-full text-center">
-                <Link
-                  href="/mypage"
-                  className="bg-yellow-100 w-full p-4 flex justify-center items-center"
-                  style={{ lineHeight: "1.5" }}
-                >
-                  ユーザーページ
-                </Link>
-              </li>
+              {user.name && (
+                <>
+                  <li className="w-full text-center">
+                    <Link
+                      href="/talkDeck/new"
+                      className="bg-yellow-100 w-full p-4 flex justify-center items-center"
+                      style={{ lineHeight: "1.5" }}
+                    >
+                      新規作成
+                    </Link>
+                  </li>
+                  <li className="w-full text-center">
+                    <Link
+                      href="/talkDeck/mydeck"
+                      className="bg-yellow-100 w-full p-4 flex justify-center items-center"
+                      style={{ lineHeight: "1.5" }}
+                    >
+                      マイデッキ
+                    </Link>
+                  </li>
+                  <li className="w-full text-center">
+                    <Link
+                      href="/talkDeck/likes"
+                      className="bg-yellow-100 w-full p-4 flex justify-center items-center"
+                      style={{ lineHeight: "1.5" }}
+                    >
+                      お気に入り
+                    </Link>
+                  </li>
+                  <li className="w-full text-center">
+                    <Link
+                      href="/mypage"
+                      className="bg-yellow-100 w-full p-4 flex justify-center items-center"
+                      style={{ lineHeight: "1.5" }}
+                    >
+                      ユーザーページ
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </div>
--- front/src/components/layout/index.ts
+++ front/src/components/layout/index.ts
@@ -1,6 +1,6 @@
 import Header from "./header";
 import Footer from "./footer";
-import MainLayout from "./mainLyout";
+import MainLayout from "./mainLayout";
 import SiteName from "./siteName/intex";
 
 export { Header, Footer, MainLayout, SiteName };
