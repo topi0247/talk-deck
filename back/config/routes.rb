3a329613e42aaa20598ed5242123daa225394f53
topi0247 <karubona.ra247@gmail.com>
2024-08-28 16:22:40 +0900

    feat:ルーティング設定とJWT設定

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/auth/:provider/callback', to: 'api/v1/sessions#create'
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: %i[show update]
+      resources :situations, only: %i[index show create]
+      resources :targets, only: %i[index create]
+      get 'situations/all_count', to: 'situations#all_count'
+    end
+  end
 end
